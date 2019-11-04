3d567472e62f6d66ca556b951915d240f59d37a7
gitboyzorro5 <kibetleonard8@gmail.com>
2019-11-04 17:57:30 +0300

    added Division header file

---
+++ Division.h
@@ -0,0 +1,16 @@
+#ifndef DIVISION_H
+#define DIVISION_H
+
+#include <string>
+
+class Division
+{
+    std::string name;
+    std::string country;
+    int total_matches;
+
+};
+
+
+
+#endif // DIVISION_H

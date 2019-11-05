7e1eaeaf3ec537238d7c4c9b6e388d810de7f035
gitboyzorro5 <kibetleonard8@gmail.com>
2019-11-05 22:23:06 +0300

    moved Division header file and added code in Team.h

--- include/Team.h
+++ include/Team.h
@@ -2,15 +2,24 @@
 #define TEAM_H
 
 #include <string>
-
+#include <vector>
 class Team
 {
 
-std::string name;
-int matches_played;
-int goals_scored;
+public:
+	Team();
+	void print_players(std::vector<std::string>);
+	void add_player(std::vector<std::string>,std::string);
 
 
+private:
+	std::string name;
+	int matches_played;
+	int full_time_home_goals;
+	int full_time_away_goals;
+	int full_time_home_concede;
+	int full_time_away_concede;
+	std::vector<std::string> players;
 
 };
 
